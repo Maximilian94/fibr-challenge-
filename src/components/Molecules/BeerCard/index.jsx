02cd1b26b008385686b010abbc2bb1870ab4810e
Maximilian94 <mkadenfilgo@gmail.com>
2022-01-19 15:29:20 -0300

    LinkStyle

--- src/components/Molecules/BeerCard/index.jsx
+++ src/components/Molecules/BeerCard/index.jsx
@@ -8,17 +8,21 @@ const BeerCard = (props) => {
 	const { name, tagline, image_url, id } = props.beerData;
 	console.log(name);
 	return (
-		<Link to={`/beer-detail/${id}`}>
+		<Link
+			style={{ textDecoration: "none", color: "black" }}
+			to={`/beer-detail/${id}`}
+		>
 			<div
 				style={{
 					borderRadius: 10,
 					background: "white",
 					padding: 10,
 					margin: 10,
-					width: 300,
+					width: 320,
 					height: 200,
 					boxShadow: "rgba(0, 0, 0, 0.16) 0px 1px 4px",
 				}}
+				className="card"
 			>
 				<Grid container>
 					<Grid item xs={6}>
@@ -30,11 +34,14 @@ const BeerCard = (props) => {
 						style={{
 							display: "flex",
 							flexDirection: "column",
-							justifyContent: "space-evenly",
 						}}
 					>
-						<Typography>{name}</Typography>
-						<Typography>{tagline}</Typography>
+						<Typography variant="h6" style={{ marginTop: 20 }} align="left">
+							{name}
+						</Typography>
+						<Typography style={{ marginTop: 20 }} align="left">
+							{tagline}
+						</Typography>
 					</Grid>
 				</Grid>
 			</div>
